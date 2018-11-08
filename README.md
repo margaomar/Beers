091ce188d3dd9fc196fa3d6a22233df24922a75a
Marga Omar <margaomar@gmail.com>
2018-11-08 22:38:28 +0000

    change ph color class + readme.md

---
+++ README.md
@@ -0,0 +1,70 @@
+# Project: Beers Api Project.
+## About this project
+You can see this project on-line, going to [Beers!](http://margaomar.github.io/BeersApi/index.html)
+
+----
+## Goals of the site
+This site has been created to learn how to create a dinamic site that allow users to search through the different beers, brands and tipologies from the Beers API. 
+
+> Hand-Crafter Beers and more
+Beer Api offers you a wide range of beers and get information about their ingredientes, qualities and features. Made with &#10084; love.
+
+----
+## Technology used
+JQuery | AJAX | API's | JS | ES6 | Babel | HTML5 | CSS3 | Sass | Gulp | Git | Github | 
+
+
+----
+## Personal Learnings
+
+1. JQuery
+2. JS 
+3. ES6 compiled into ES5 using Babel 
+4. ES5 classes, constructor and methods
+5. default import and export
+6. Ajax, JSON and APIs Gulp
+7. Gulp
+
+
+
+----
+## Link to the files on Github
+* **[Beers Api on Github](https://github.com/margaomar/BeersApi)**
+
+<!-- ----
+## Media Query Mobile First Break Points 
+
+* 425px
+
+* 600px
+
+* 1200px
+
+----
+## Device Screenshots
+
+**Mobile Device**
+----
+<img src="screenshoots/movile-closed-320px.png" width="320px" />
+<img src="screenshoots/movile-open-320px.jpg" width="320px" />
+
+**Tablet Device**
+----
+<img src="screenshoots/tablet-closed-600px.png" width="600px" />
+<img src="screenshoots/tablet-open-600px.jpg" width="600px" />
+
+
+**Desktop Device**
+----
+<img src="screenshoots/desktop-closed-1200px.png" width="1200px" />
+<img src="screenshoots/desktop-open-1200px.png" width="1200px" />
+ -->
+
+----
+## Dead Line for the project
+* 8-Nov-2018
+
+----
+## Project developed by 
+ **[Marga Omar]
+(https://margaomar.com)**
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import $ from './jquery';
-    beerHomeBeer(); // call the homepage
+    beerHomeBeer(); // call the homepage first
 
-    class Beer { //class
+    class Beer { //class 
         constructor (name, description, image_url, abv, ibu, ph, tagline, ingredients) {
         this.name = name;
         this.description = description;
@@ -14,68 +14,67 @@ import $ from './jquery';
         this.hops = ingredients.hops;
         this.yeast = ingredients.yeast
         }
-        getBeerData(){ //method
-            return `<article class="item-containers"><figure><img src=${this.image_url} alt="${this.name}"></figure>
-            <div class="item-description"><h1>${this.name}</h1>
-            <p>${this.description}</p></div>
-            <ul class="item-values"><li>ABV<span>${this.abv}</span></li><li>IBU<span>${this.ibu}</span></li><li class="alert">pH<span>${this.ph}</span></li></ul></article>`
-        }   
-        // ${this.name}
-        getBeerDetData(){
-            
-            // for (let i = 0; i < yeast.length; i++) {
-                // yeastString += `${yeast[i].name},`
-            // }
-            // console.log(maltString)
-            // console.log(hopsString)
-            // console.log(yeastString)
-            return `<article class="section-detail">
-                        <div class="item-containers quick-section">
-                            <figure>
-                                <img src=${this.image_url} alt="${this.name}"></figure>
-                            <div class="item-description">
-                            <h1>${this.name}</h1>
-                            <h2>${this.tagline}</h2>
-                            <p>${this.description}</p>
-                            </div>
-                            <ul class="item-values">
+        getColorClass(ph) { //loop alert colour on the different beers ph values
+
+            if (ph > 5) {
+                return 'alert-hight';
+            }
+            else if (ph < 5 && ph >= 4.4){
+                return 'alert-medium';
+            }
+            else if(ph < 4.4 ) {
+                return 'alert-low';
+            }
+        } 
+        getBeerData(){ //method homepage
+
+            let phColor = this.getColorClass(this.ph);
+            return `<article class="item-containers">
+                        <figure><img src=${this.image_url} alt="${this.name}"></figure>
+                        <div class="item-description"><h1>${this.name}</h1><p>${this.description}</p></div>
+                        <ul class="item-values">
                             <li>ABV<span>${this.abv}</span></li>
                             <li>IBU<span>${this.ibu}</span></li>
-                            <li class="alert">pH<span>${this.ph}</span></li>
+                            <li class="${phColor}">pH<span>${this.ph}</span></li>
+                        </ul>
+                    </article>`
+        }   
+        getBeerDetData(){ //method ramdom
+            
+            let phColor = this.getColorClass(this.ph);
+            return `<article class="section-detail">
+                        <div class="item-containers quick-section">
+                            <figure><img src=${this.image_url} alt="${this.name}"></figure>
+                            <div class="item-description">
+                                <h1>${this.name}</h1>
+                                <h2>${this.tagline}</h2>
+                                <p>${this.description}</p>
+                            </div>
+                            <ul class="item-values">
+                                <li>ABV<span>${this.abv}</span></li>
+                                <li>IBU<span>${this.ibu}</span></li>
+                                <li class="${phColor}">pH<span>${this.ph}</span></li>
                             </ul>
                             <button class="btn-item-modal">Ingredients</button>
                             <button class="btn-section-another">Give me another beer</button>
                         </div>
-                        
                     </article>`
 
         }
         getBeerModal(){ //pop up information
+
             let maltString = '';
             let hopsString = '';
             let yeastString = this.yeast;
-
             let malt = this.malt;
             let hops = this.hops;
 
-            // console.log("a", this.malt, this.hops, this.yeast)
-            // for (let i = 0; i < 1; i++) {
-            //     // let maltName = malt[i].name;
-            //     // console.log(malt[i].value);
-            //     // console.log(malt[i].unit);
-            //     maltString += `${malt[i].name} (${malt[i].amount.value} ${malt[i].amount.unit}),`
-            // }
             maltString += `${malt[0].name} (${malt[0].amount.value} ${malt[0].amount.unit})`
 
-            // console.log("maltString", maltString);
-            
-            // for (let i = 0; i < hops.length; i++) {
-            //     hopsString += `${hops[i].name} (${hops[i].amount.value} ${hops[i].amount.unit}), `
-            // }
             hopsString += `${hops[0].name} (${hops[0].amount.value} ${hops[0].amount.unit})`
 
             return `<div class="modal-container">
-                        <h3 class="modal-tittle">Ingredients <span class="modal-closed">X</span></h3>
+                        <h3 class="modal-tittle">INGREDIENTS <span class="modal-closed">x</span></h3>
                         <div class="modal-description">
                             <h3>Malt:</h3>
                             <p>${maltString}</p>
@@ -87,9 +86,10 @@ import $ from './jquery';
                     </div>`
         }
         
-    }//close class Beer
+    }//ends class Beer
     
-    function getPickABeer(){
+    function getPickABeer(){ //method pick a beer form
+
         return `<article class="section-detail">
                     <div class="item-containers pick-section">
                     <h1 class="tittle">Pick a Beer</h1>
@@ -116,47 +116,20 @@ import $ from './jquery';
                 </article>`
     }
 
-
-    //change alert colour on the different beers ph values
-    function $alertColour(alertColour){              
-        // console.log(alertColour);
-        if(alertColour > 5.1){
-            $('.alert').addClass("alert-hight");
-        }else if(alertColour < 5.1 && alertColour > 4.3){
-            $('.alert').addClass("alert-medium");
-        }else if(alertColour <= 4.3){
-            $('.alert').addClass("alert-low");
-        }else{
-            alert('this is not possible');
-        }
-    };
-    
-
     // set some initial variables
     var $beersWrap = $('.grid-container');
-   
-  // when the form is submitted
-    //$(".all").on('click', function(){
-        //button is active
-        // $(".all").addClass("active", 10000);
-    
-    //Add active to main page
-    // $('.beers').siblings().removeClass("active");
-    // $('.beers').addClass("active");
-
     //Add active class when press on beers
     $(".beers").on('click', function() {
         $(this).siblings().removeClass("active");
         $(this).addClass("active");
         beerHomeBeer(); //call the homepage
-
+        $('.pagination').removeClass('hide');
         // add second menu again
         $('.main-menu').removeClass('hide');
         $('header').removeClass('hide-nav');
     });
     
     //Get homepage
-    // declare url for the API
     function beerHomeBeer(){
         var url = "https://api.punkapi.com/v2/beers?page=1&per_page=6"; //Get beers homepage
         $.ajax({  
@@ -167,12 +140,7 @@ import $ from './jquery';
                 for(let i=0; i<data.length; i++){
                     //calling the data by the class Beer
                     let beer = new Beer(data[i].name, data[i].description, data[i].image_url, data[i].abv, data[i].ibu, data[i].ph, data[i].tagline, data[i].ingredients);
-                    $beersWrap.append(beer.getBeerData());
-                
-                    //change alert value colour into the loop
-                    let alertColour = data[i].ph;
-                    $alertColour(alertColour);
-
+                    $beersWrap.append(beer.getBeerData());                     
                     //add active class
                     $('.beers').addClass("active");
                 };
@@ -209,10 +177,6 @@ import $ from './jquery';
                 //calling the data by the class Beer
                 let beer = new Beer(data[i].name, data[i].description, data[i].image_url, data[i].abv, data[i].ibu, data[i].ph, data[i].tagline, data[i].ingredients);
                 $beersWrap.append(beer.getBeerData());
-
-                //change alert value colour into the loop
-                let alertColour = data[i].ph;
-                $alertColour(alertColour);
             }
         });
     });
@@ -222,6 +186,9 @@ import $ from './jquery';
     function random(){ //Quick find (Random) + modal, using getBeerDetData
         $(this).siblings().removeClass("active");
         $(this).addClass("active");
+        //remove second menu and pag
+        $('.main-menu').addClass('hide');
+        $('header').addClass('hide-nav');
         $(".pagination").addClass("hide");
 
         var url = "https://api.punkapi.com/v2/beers/random";
@@ -232,25 +199,11 @@ import $ from './jquery';
             $beersWrap.empty();
             for(let i=0; i<1; i++){
                 //calling the data by the class Beer
-                // console.log(data);
-                // console.log(i);
                 let beer = new Beer(data[i].name, data[i].description, data[i].image_url, data[i].abv, data[i].ibu, data[i].ph, data[i].tagline, data[i].ingredients);
                 $beersWrap.append(beer.getBeerDetData());
 
-                //change alert value colour into the loop
-                let alertColour = data[i].ph;
-                $alertColour(alertColour);
-
-                //remove second menu
-                $('.main-menu').addClass('hide');
-                $('header').addClass('hide-nav');
-
-                //get the modal info
-                // $beersWrap.append(beer.getBeerDetData());
-
                 //getBeerModal()
                 $('.btn-item-modal').on('click', function() {
-                    // $(this).getBeerModal().trigger();
                     $('.section-detail').append(beer.getBeerModal());
                     $('.modal-backdrop').addClass('show');
                 });
@@ -258,44 +211,20 @@ import $ from './jquery';
                     $('.modal-container').addClass('hide');
                     $('.modal-backdrop').removeClass('show');
                 });
-                
             }
         });
     };
-    // random(); // call the function to be able to works when you click on random
-    
 
-    
-
-
-    // $('.grid-container').on('click', ".btn-section-another", function() { // Another beer random button call
-    //     random(); //call random
-    // });
     $('.grid-container').on('click', ".btn-section-another", random);// Another beer random button call
-
-    // Pick a beer section
-    $('.pick-beer').on('click', function(){
-        $beersWrap.empty();
-        $beersWrap.append(getPickABeer());
+    $('.pick-beer').on('click', function(){ // Pick a beer section
         $(this).siblings().removeClass("active");
         $(this).addClass("active");
+
+        $beersWrap.empty();
+        $beersWrap.append(getPickABeer());
+        
+        //remove second menu and pag
+        $('.main-menu').addClass('hide');
+        $('header').addClass('hide-nav');
+        $(".pagination").addClass("hide");
     });
-    console.log(getPickABeer);
-
-    // $(this).siblings().removeClass("active");
-    // $(this).addClass("active");
-    // $(".pagination").addClass("hide");
-
-    // .fail(function() {
-    //     $('.user-name').append('Sorry there was an error. Try again.');
-    //  });
-
-
-
-
-
-// function newFunction() {
-//     let getBeerData;
-//     return getBeerData;
-// }
-
